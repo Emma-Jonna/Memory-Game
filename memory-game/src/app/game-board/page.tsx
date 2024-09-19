546e809c69c3b53057c51323930a60857d4aaa08
Emma-Jonna <johanna.emma.pihl@gmail.com>
2024-09-19 13:20:46 +0200

    update game board page
    updated game board with new context and display all the cards for the game

--- memory-game/src/app/game-board/page.tsx
+++ memory-game/src/app/game-board/page.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 // import styles from "./page.module.css";
 
 import {GameDifficultyContext} from "../contexts/DifficultyContext";
+import {DeckOfCardsContext} from "../contexts/DeckOfCardsContext";
+import {useContext, useEffect, useState} from "react";
+import {redirect} from "next/navigation";
 
 export default function GameBoard() {
-  return (
+  const {chosendifficulty, changeDifficultyTo, gameDifficulties} = useContext(
+    GameDifficultyContext
+  ) as GameDifficultyContext;
+  const {getRandomCardsForGame, deckOfCards} = useContext(DeckOfCardsContext) as DeckOfCardsContext;
+
+  if (chosendifficulty == null) {
+    redirect("/");
+  }
+
+  // console.log(arrayOfCards);
+
+  return !deckOfCards ? null : (
     <main>
       <div>Game Board</div>
+      {deckOfCards.map((data, index) => {
+        return <div key={index}>{data.name}</div>;
+      })}
     </main>
   );
 }
